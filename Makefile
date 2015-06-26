1a94e5de3b1aaffa3b8caf1e9324ca5ec8834c2e
Sebastian <sebablasko@gmail.com>
2015-06-26 16:03:02 -0300

    added files

--- Makefile
+++ Makefile
@@ -2,18 +2,18 @@ all: server client
 prof: serverProfiling client
 
 server: server.o ../ssocket/ssocket.o
-	gcc -o3 server.o ../ssocket/ssocket.o -o server -lpthread
+	gcc -o3 server.o ../ssocket/ssocket.o -o serverTesis -lpthread
 
 serverProfiling: server.o ../ssocket/ssocket.o
-	gcc -g -o3 server.o ../ssocket/ssocket.o -o server -lpthread
+	gcc -g -o3 server.o ../ssocket/ssocket.o -o serverTesis -lpthread
 
 rm_server:
-	rm server server.o
+	rm serverTesis server.o
 
 client: client.o ../ssocket/ssocket.o
-	gcc -o3 client.o ../ssocket/ssocket.o -o client -lpthread
+	gcc -o3 client.o ../ssocket/ssocket.o -o clientTesis -lpthread
 
 rm_client:
-	rm client client.o
+	rm clientTesis client.o
 
 clean: rm_client rm_server
--- run.sh
+++ run.sh
@@ -4,16 +4,16 @@ echo "Compilando..."
 make all
 echo "Done"
 
-salida=reuseport_sockets_stress.csv
-
 MAX_PACKS=1000000
-total_sockets="1 2 4 8 16 24 32 64 128"
+total_sockets="1 2 4 8 12 16 24 32 48 64 128"
 num_threads_per_socket=1
 total_clients=4
 num_port=1820
 repetitions=20
 
-echo "Ejecutando Prueba..."
+salida=reuseport_sockets_stress_SOSched.csv
+
+echo "Ejecutando Prueba... $salida"
 for num_sockets in $total_sockets
 do
 
@@ -26,18 +26,17 @@ do
 		echo "" > aux
 		for ((k=0 ; $k<$num_sockets ; k++))
 		{
-			#./server --packets $(($MAX_PACKS/num_sockets)) --threads $num_threads_per_socket --port $num_port --reuseport >> aux &
-			./server --scheduler dummySched --setcpu $(($k%$(nproc))) --packets $(($MAX_PACKS/num_sockets)) --threads $num_threads_per_socket --port $num_port --reuseport >> aux &
+			./serverTesis --packets $(($MAX_PACKS/num_sockets)) --threads $num_threads_per_socket --port $num_port --reuseport >> aux &
 		}
 
 		sleep 1
 
 		for ((j=1 ; $j<=$total_clients ; j++))
 		{
-			./client --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $num_port > /dev/null &
+			./clientTesis --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $num_port > /dev/null &
 		}
 
-		wait $(pgrep 'server')
+		wait $(pgrep 'serverTesis')
 
 		linea="$linea$(cat aux)"
 	}
@@ -46,5 +45,84 @@ do
 	echo ""
 	rm aux
 done
+# fix the format
+sed 's/;//g' $salida | sed 's/,//g' | sed 's/\./,/g' > 'fix_'$salida
+
+
+salida=reuseport_sockets_stress_EtitativeSched.csv
+
+echo "Ejecutando Prueba... $salida"
+for num_sockets in $total_sockets
+do
+
+	linea=$num_sockets";"
+	echo "sockets: $num_sockets"
+
+	for ((i=1 ; $i<=$repetitions ; i++))
+	{
+		echo "Repeticion $i"
+		echo "" > aux
+		for ((k=0 ; $k<$num_sockets ; k++))
+		{
+			./serverTesis --scheduler dummySched --setcpu $(($k%$(nproc))) --packets $(($MAX_PACKS/num_sockets)) --threads $num_threads_per_socket --port $num_port --reuseport >> aux &
+		}
+
+		sleep 1
+
+		for ((j=1 ; $j<=$total_clients ; j++))
+		{
+			./clientTesis --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $num_port > /dev/null &
+		}
+
+		wait $(pgrep 'serverTesis')
+
+		linea="$linea$(cat aux)"
+	}
+	echo $linea
+	echo "$linea" >> $salida
+	echo ""
+	rm aux
+done
+# fix the format
+sed 's/;//g' $salida | sed 's/,//g' | sed 's/\./,/g' > 'fix_'$salida
+
+
+salida=reuseport_sockets_stress_All0Sched.csv
+
+echo "Ejecutando Prueba... $salida"
+for num_sockets in $total_sockets
+do
+
+	linea=$num_sockets";"
+	echo "sockets: $num_sockets"
+
+	for ((i=1 ; $i<=$repetitions ; i++))
+	{
+		echo "Repeticion $i"
+		echo "" > aux
+		for ((k=0 ; $k<$num_sockets ; k++))
+		{
+			./serverTesis --scheduler dummySched --setcpu 0 --packets $(($MAX_PACKS/num_sockets)) --threads $num_threads_per_socket --port $num_port --reuseport >> aux &
+		}
+
+		sleep 1
+
+		for ((j=1 ; $j<=$total_clients ; j++))
+		{
+			./clientTesis --packets $(($MAX_PACKS*10)) --ip 127.0.0.1 --port $num_port > /dev/null &
+		}
+
+		wait $(pgrep 'serverTesis')
+
+		linea="$linea$(cat aux)"
+	}
+	echo $linea
+	echo "$linea" >> $salida
+	echo ""
+	rm aux
+done
+# fix the format
+sed 's/;//g' $salida | sed 's/,//g' | sed 's/\./,/g' > 'fix_'$salida
 
 make clean
+

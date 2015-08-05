fdba34b42be924164a9ba0250b635c76332abe2e
Sebastian <sebablasko@gmail.com>
2015-08-05 15:22:06 -0300

    corregidos nombres de archivos

--- run.sh
+++ run.sh
@@ -5,23 +5,27 @@ make all
 echo "Done"
 
 MAX_PACKS=1000000
-total_sockets="1 2 4 8 16 24 32 48 64"
-total_num_threads_per_socket="1 2 4 8"
+total_sockets="2 3"
+total_num_threads_per_socket="3 4"
 total_clients=4
 num_port=1820
-repetitions=20
+repetitions=2
 
 
 for num_threads_per_socket in $total_num_threads_per_socket
 do
 
-	salida="reuseport_sockets_stress_SOSched_"$num_threads_per_socket"Threads_per_socket.csv"
+
+	if (($num_threads_per_socket >= 0 & $num_threads_per_socket < 10)); then salida="reuseport_sockets_stress_SOSched_00"$num_threads_per_socket"_ThreadsBySocket.csv"; fi
+	if (($num_threads_per_socket >= 10 & $num_threads_per_socket < 100)); then salida="reuseport_sockets_stress_SOSched_0"$num_threads_per_socket"_ThreadsBySocket.csv"; fi
+	if (($num_threads_per_socket >= 100)); then salida="reuseport_sockets_stress_SOSched_"$num_threads_per_socket"_ThreadsBySocket.csv"; fi
 
 	echo "Ejecutando Prueba... $salida"
 	for num_sockets in $total_sockets
 	do
 
-		linea=$num_sockets";"
+		#linea=$num_sockets";"
+		linea=""
 		echo "sockets: $num_sockets, threads: $num_threads_per_socket"
 
 		for ((i=1 ; $i<=$repetitions ; i++))
@@ -30,7 +34,7 @@ do
 			echo "" > aux
 			for ((k=0 ; $k<$num_sockets ; k++))
 			{
-				./serverTesis --packets $(($MAX_PACKS/num_sockets)) --threads $num_threads_per_socket --port $num_port --reuseport >> aux &
+				./serverTesis --packets $(($MAX_PACKS/num_sockets)) --threads $num_threads_per_socket --port $num_port --reuseport >> aux &	
 			}
 
 			sleep 1
@@ -53,13 +57,17 @@ do
 	sed 's/;//g' $salida | sed 's/,//g' | sed 's/\./,/g' > 'fix_'$salida
 
 
-	salida="reuseport_sockets_stress_EquitativeSched_"$num_threads_per_socket"Threads_per_socket.csv"
+
+	if (($num_threads_per_socket >= 0 & $num_threads_per_socket < 10)); then salida="reuseport_sockets_stress_EquitativeSched_00"$num_threads_per_socket"_ThreadsBySocket.csv"; fi
+	if (($num_threads_per_socket >= 10 & $num_threads_per_socket < 100)); then salida="reuseport_sockets_stress_EquitativeSched_0"$num_threads_per_socket"_ThreadsBySocket.csv"; fi
+	if (($num_threads_per_socket >= 100)); then salida="reuseport_sockets_stress_EquitativeSched_"$num_threads_per_socket"_ThreadsBySocket.csv"; fi
 
 	echo "Ejecutando Prueba... $salida"
 	for num_sockets in $total_sockets
 	do
 
-		linea=$num_sockets";"
+		#linea=$num_sockets";"
+		linea=""
 		echo "sockets: $num_sockets, threads: $num_threads_per_socket"
 
 		for ((i=1 ; $i<=$repetitions ; i++))
@@ -91,13 +99,17 @@ do
 	sed 's/;//g' $salida | sed 's/,//g' | sed 's/\./,/g' > 'fix_'$salida
 
 
-	salida="reuseport_sockets_stress_All0Sched_"$num_threads_per_socket"Threads_per_socket.csv"
+
+	if (($num_threads_per_socket >= 0 & $num_threads_per_socket < 10)); then salida="reuseport_sockets_stress_All0Sched_00"$num_threads_per_socket"_ThreadsBySocket.csv"; fi
+	if (($num_threads_per_socket >= 10 & $num_threads_per_socket < 100)); then salida="reuseport_sockets_stress_All0Sched_0"$num_threads_per_socket"_ThreadsBySocket.csv"; fi
+	if (($num_threads_per_socket >= 100)); then salida="reuseport_sockets_stress_All0Sched_"$num_threads_per_socket"_ThreadsBySocket.csv"; fi
 
 	echo "Ejecutando Prueba... $salida"
 	for num_sockets in $total_sockets
 	do
 
-		linea=$num_sockets";"
+		#linea=$num_sockets";"
+		linea=""
 		echo "sockets: $num_sockets, threads: $num_threads_per_socket"
 
 		for ((i=1 ; $i<=$repetitions ; i++))
